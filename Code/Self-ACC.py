7b56c71634a330e7ae8d06d15a335ed97ff99d26
YZKQDU <119778963+YZKQDU@users.noreply.github.com>
2023-10-03 10:33:17 +0800

    Update Self-ACC.py

--- Code/Self-ACC.py
+++ Code/Self-ACC.py
@@ -14,6 +14,19 @@ from tensorflow.keras.optimizers import RMSprop
 import seaborn as sns
 from tensorflow.keras import models
 import pandas as pd
+import numpy as np
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Dense, Add
+from tensorflow.keras.wrappers.scikit_learn import KerasRegressor
+from sklearn.model_selection import cross_val_score
+from sklearn.datasets import make_regression
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense
+from tensorflow.keras.models import Model
+from sklearn.model_selection import KFold
+from sklearn.metrics import mean_squared_error
 num=30 
 class ResNetBlock(tf.keras.Model):
     def __init__(self,filter_num,stride=1):
@@ -87,85 +100,30 @@ end=tf.keras.layers.Flatten()(end)
 end=tf.keras.layers.Dense(512, activation='relu')(end)
 end=tf.keras.layers.Dense(256, activation='relu')(end)
 output1=tf.keras.layers.Dense(num, activation='softmax')(end)
-model = tf.keras.Model(inputs=input1, outputs=output1)
-input11=np.load('D:\\dataset\\data30\\train_decoder.npy')
-train_labels = np.load('D:\\dataset\\data30\\train_label.npy')
-iinput1=np.load('D:\\dataset\\data30\\test_decoder.npy')
-ttrain_labels = np.load('D:\\dataset\\data30\\test_label.npy')
-checkpoint_path = "D:\\dataset\\data30\\weight\\{epoch}.hdf5"
+input11=np.load(r'D:\\paper1\\weight\\all.npy')
+train_labels = np.load(r'D:\\paper1\\weight\\label.npy')
+checkpoint_path = "D:\\dataset\\data30\\{epoch}.hdf5"
 checkpoint_dir = os.path.dirname(checkpoint_path)  
-# model.load_weights("D:\\dataset\\data0_3\\15.hdf5")
-# optimizer = tf.keras.optimizers.Adam(0.001)
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy', 
-              metrics = ['categorical_accuracy',tf.keras.metrics.Precision(),tf.keras.metrics.Recall(),tf.keras.metrics.FalseNegatives(),tf.keras.metrics.FalsePositives()]
-              )
 cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                   save_weights_only=True,
                                                   verbose=1)
-model.load_weights('D:\\dataset\\data30\\weight\\1.hdf5')
-# H = model.fit(input11,train_labels,batch_size=50,validation_data=(iinput1,ttrain_labels),
-#                               epochs=15,
-#                               # validation_steps=50,
-#                               verbose=1,
-#                               # callbacks=[cp_callback],
-#                               shuffle=True
-#                               )
-
-
-
-# loss,accuracy = model.evaluate(iinput1,ttrain_labels,batch_size=50)
-# print('accuracy',accuracy)
-num=35
-
-num2=2606
-layer_outputs = [layer.output for layer in model.layers[:num]]
-activation_model = models.Model(inputs=model.input, outputs=layer_outputs) 
-img=iinput1[num2]
-plt.figure()
-plt.imshow(img)
-input_image=tf.expand_dims(img, 0) 
-activations = activation_model.predict(input_image) 
-print(activations[0].shape)
-# print(iinput1[0].shape)
-plt.matshow(activations[3][0,:,:,0], cmap='viridis') 
-plt.matshow(activations[3][0,:,:,10], cmap='viridis') 
-layer_names = []
-for layer in model.layers[:num]:
-    layer_names.append(layer.name) 
-
-images_per_row=16 
-
-for layer_name, layer_activation in zip (layer_names[:num], activations[:num]):
-    n_feature = layer_activation.shape[-1]
-    size = layer_activation.shape[1]
-    n_cols = n_feature//images_per_row
-    display_grid = np.zeros((size*n_cols, images_per_row*size))
-    for col in range(n_cols): 
-        for row in  range (images_per_row): 
-            # print(layer_activation.shape)
-            # print(col*images_per_row+row)
-            channel_image = layer_activation[0,:,:,col*images_per_row+row] 
-            channel_image -= channel_image.mean() 
-            channel_image /= channel_image.std()
-            channel_image *=64
-            channel_image +=128
-            channel_image = np.clip(channel_image, 0, 255).astype('uint8')
-            # print(channel_image.shape)
-            # print(display_grid[col*size:(col+1)*size, row*size:(row+1)*size].shape)
-            display_grid[col*size:(col+1)*size, row*size:(row+1)*size] = channel_image 
-    scale = 1./size 
-    plt.figure(figsize=(scale*display_grid.shape[1], scale*display_grid.shape[0]))
-    plt.title(layer_name)
-    plt.grid(False)
-    plt.imshow(display_grid, aspect='auto', cmap='viridis')
-
-
-
-
-
-
-
+kfold = KFold(n_splits=10,shuffle = True)  #K-fold cross validation
+for train_index, test_index in kfold.split(input11):
+    tf.keras.backend.clear_session()
+    X_train, X_test = input11[train_index], input11[test_index]
+    Y_train, Y_test = train_labels[train_index], train_labels[test_index]
+    model = tf.keras.Model(inputs=input1, outputs=output1)
+    model.compile(optimizer='adam',
+                  loss='categorical_crossentropy', 
+                  metrics = ['categorical_accuracy']
+                  )
+    H = model.fit(X_train,Y_train,validation_data=(X_test,Y_test),batch_size=50,
+                                  epochs=1,
+                                  # validation_steps=50,
+                                  verbose=1,
+                                  # callbacks=[cp_callback],
+                                  shuffle=True
+                                  )
 
 
 
